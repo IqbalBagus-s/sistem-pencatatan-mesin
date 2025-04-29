3fed3b54e120534da705b9ed4a8342392c2d80c3
iqbal.bs_ <119472842+IqbalBagus-s@users.noreply.github.com>
2025-04-29 11:28:44 +0700

    update create and edit component buttons

--- resources/views/vacuum_cleaner/edit.blade.php
+++ resources/views/vacuum_cleaner/edit.blade.php
@@ -310,24 +310,7 @@
                 </div>
             </div>
 
-            <div class="flex items-center justify-between mt-8">
-                <a href="{{ route('vacuum-cleaner.index') }}" class="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
-                    <span class="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-                        </svg>
-                        Kembali
-                    </span>
-                </a>
-                <div class="flex gap-2">
-                    <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
-                        </svg>
-                        Simpan Perubahan
-                    </button>
-                </div>
-            </div>
+            @include('components.edit-form-buttons', ['backRoute' => route('vacuum-cleaner.index')])
         </form>
     </div>
 @endsection
