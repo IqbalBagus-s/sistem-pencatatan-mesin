f8088342746e3f26fa97a7f6f3c544d3ea07296b
iqbal.bs_ <119472842+IqbalBagus-s@users.noreply.github.com>
2025-05-04 19:28:57 +0700

    rewrite code for routes by using resource to get more simple

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\CapliningController;
 use App\Http\Controllers\VacumCleanerController;
 use App\Http\Controllers\SlittingController;
 
-
+// Route Publik
 Route::get('/', function () {
     return view('auth.login');
 });
@@ -27,129 +27,33 @@ Route::get('/login', function () {
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-
+// Route yang memerlukan autentikasi
 Route::middleware(['auth:approver,checker'])->group(function () {
-    // Route Dashboard
+    // Dashboard
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     
-    // Route Mesin Air Dryer
-    Route::get('/air-dryer', [AirDryerController::class, 'index'])->name('air-dryer.index');
-    Route::get('/air-dryer/create', [AirDryerController::class, 'create'])->name('air-dryer.create');
-    Route::post('/air-dryer', [AirDryerController::class, 'store'])->name('air-dryer.store');
-    Route::get('/air-dryer/{id}/edit', [AirDryerController::class, 'edit'])->name('air-dryer.edit');
-    Route::put('/air-dryer/{id}', [AirDryerController::class, 'update'])->name('air-dryer.update');
-    Route::get('/air-dryer/{id}', [AirDryerController::class, 'show'])->name('air-dryer.show');
-    Route::post('/air-dryer/{id}/approve', [AirDryerController::class, 'approve'])->name('air-dryer.approve');
-    // Route download PDF
-    Route::get('/air-dryer/{id}/download-pdf', [AirDryerController::class, 'downloadPdf'])->name('air-dryer.downloadPdf');
-
-    // Route Mesin Water Chiller
-    Route::get('/water-chiller', [WaterChillerController::class, 'index'])->name('water-chiller.index');
-    Route::get('/water-chiller/create', [WaterChillerController::class, 'create'])->name('water-chiller.create');
-    Route::post('/water-chiller', [WaterChillerController::class, 'store'])->name('water-chiller.store');
-    Route::get('/water-chiller/{id}/edit', [WaterChillerController::class, 'edit'])->name('water-chiller.edit');
-    Route::put('/water-chiller/{id}', [WaterChillerController::class, 'update'])->name('water-chiller.update');
-    Route::get('/water-chiller/{id}', [WaterChillerController::class, 'show'])->name('water-chiller.show');
-    Route::post('/water-chiller/{id}/approve', [WaterChillerController::class, 'approve'])->name('water-chiller.approve');
-    // Route download PDF
-    Route::get('/water-chiller/{id}/download-pdf', [WaterChillerController::class, 'downloadPdf'])->name('water-chiller.downloadPdf');
+    // Daftar kontroler mesin dengan operasi CRUD yang sama
+    $controllers = [
+        'air-dryer' => AirDryerController::class,
+        'water-chiller' => WaterChillerController::class,
+        'compressor' => CompressorController::class,
+        'hopper' => HopperController::class,
+        'dehum-bahan' => DehumBahanController::class,
+        'giling' => GilingController::class,
+        'autoloader' => AutoloaderController::class,
+        'dehum-matras' => DehumMatrasController::class,
+        'caplining' => CapliningController::class,
+        'vacuum-cleaner' => VacumCleanerController::class,
+        'slitting' => SlittingController::class,
+    ];
     
-    // Route Mesin Compressor
-    Route::get('/compressor', [CompressorController::class, 'index'])->name('compressor.index');
-    Route::get('/compressor/create', [CompressorController::class, 'create'])->name('compressor.create');
-    Route::post('/compressor', [CompressorController::class, 'store'])->name('compressor.store');
-    Route::get('/compressor/{id}/edit', [CompressorController::class, 'edit'])->name('compressor.edit');
-    Route::put('/compressor/{id}', [CompressorController::class, 'update'])->name('compressor.update');
-    Route::get('/compressor/{id}', [CompressorController::class, 'show'])->name('compressor.show');
-    Route::post('/compressor/{id}/approve', [CompressorController::class, 'approve'])->name('compressor.approve');
-    // Route download PDF
-    Route::get('/compressor/{id}/download-pdf', [CompressorController::class, 'downloadPdf'])->name('compressor.downloadPdf');
-
-    // Route Mesin Hopper
-    Route::get('/hopper', [HopperController::class, 'index'])->name('hopper.index');
-    Route::get('/hopper/create', [HopperController::class, 'create'])->name('hopper.create');
-    Route::post('/hopper', [HopperController::class, 'store'])->name('hopper.store');
-    Route::get('/hopper/{id}/edit', [HopperController::class, 'edit'])->name('hopper.edit');
-    Route::put('/hopper/{id}', [HopperController::class, 'update'])->name('hopper.update');
-    Route::get('/hopper/{id}', [HopperController::class, 'show'])->name('hopper.show');
-    Route::post('/hopper/{id}/approve', [HopperController::class, 'approve'])->name('hopper.approve');
-    // Route download PDF
-    Route::get('/hopper/{id}/download-pdf', [HopperController::class, 'downloadPdf'])->name('hopper.downloadPdf');
-
-    // Route Mesin Dehum Bahan
-    Route::get('/dehum-bahan', [DehumBahanController::class, 'index'])->name('dehum-bahan.index');
-    Route::get('/dehum-bahan/create', [DehumBahanController::class, 'create'])->name('dehum-bahan.create');
-    Route::post('/dehum-bahan', [DehumBahanController::class, 'store'])->name('dehum-bahan.store');
-    Route::get('/dehum-bahan/{id}/edit', [DehumBahanController::class, 'edit'])->name('dehum-bahan.edit');
-    Route::put('/dehum-bahan/{id}', [DehumBahanController::class, 'update'])->name('dehum-bahan.update');
-    Route::get('/dehum-bahan/{id}', [DehumBahanController::class, 'show'])->name('dehum-bahan.show');
-    Route::post('/dehum-bahan/{id}/approve', [DehumBahanController::class, 'approve'])->name('dehum-bahan.approve');
-    // Route download PDF
-    Route::get('/dehum-bahan/{id}/download-pdf', [DehumBahanController::class, 'downloadPdf'])->name('dehum-bahan.downloadPdf');
+    // Alternatif menggunakan Resource Controller (lebih disarankan):
+    foreach ($controllers as $route => $controller) {
+        Route::resource($route, $controller);
+        
+        // Tambahkan route khusus untuk approve dan download PDF
+        Route::post("/$route/{id}/approve", [$controller, 'approve'])->name("$route.approve");
+        Route::get("/$route/{id}/download-pdf", [$controller, 'downloadPdf'])->name("$route.downloadPdf");
+    }
     
-    // Route Mesin Giling
-    Route::get('/giling', [GilingController::class, 'index'])->name('giling.index');
-    Route::get('/giling/create', [GilingController::class, 'create'])->name('giling.create');
-    Route::post('/giling', [GilingController::class, 'store'])->name('giling.store');
-    Route::get('/giling/{id}/edit', [GilingController::class, 'edit'])->name('giling.edit');
-    Route::put('/giling/{id}', [GilingController::class, 'update'])->name('giling.update');
-    Route::get('/giling/{id}', [GilingController::class, 'show'])->name('giling.show');
-    Route::post('/giling/{id}/approve', [GilingController::class, 'approve'])->name('giling.approve');
-    // Route download PDF
-    Route::get('/giling/{id}/download-pdf', [GilingController::class, 'downloadPdf'])->name('giling.downloadPdf');
-
-    // Route Mesin Giling
-    Route::get('/autoloader', [AutoloaderController::class, 'index'])->name('autoloader.index');
-    Route::get('/autoloader/create', [AutoloaderController::class, 'create'])->name('autoloader.create');
-    Route::post('/autoloader', [AutoloaderController::class, 'store'])->name('autoloader.store');
-    Route::get('/autoloader/{id}/edit', [AutoloaderController::class, 'edit'])->name('autoloader.edit');
-    Route::put('/autoloader/{id}', [AutoloaderController::class, 'update'])->name('autoloader.update');
-    Route::get('/autoloader/{id}', [AutoloaderController::class, 'show'])->name('autoloader.show');
-    Route::post('/autoloader/{id}/approve', [AutoloaderController::class, 'approve'])->name('autoloader.approve');
-    // Route download PDF
-    Route::get('/autoloader/{id}/download-pdf', [AutoloaderController::class, 'downloadPdf'])->name('autoloader.downloadPdf');
-
-    // Route Mesin Dehum Matras
-    Route::get('/dehum-matras', [DehumMatrasController::class, 'index'])->name('dehum-matras.index');
-    Route::get('/dehum-matras/create', [DehumMatrasController::class, 'create'])->name('dehum-matras.create');
-    Route::post('/dehum-matras', [DehumMatrasController::class, 'store'])->name('dehum-matras.store');
-    Route::get('/dehum-matras/{id}/edit', [DehumMatrasController::class, 'edit'])->name('dehum-matras.edit');
-    Route::put('/dehum-matras/{id}', [DehumMatrasController::class, 'update'])->name('dehum-matras.update');
-    Route::get('/dehum-matras/{id}', [DehumMatrasController::class, 'show'])->name('dehum-matras.show');
-    Route::post('/dehum-matras/{id}/approve', [DehumMatrasController::class, 'approve'])->name('dehum-matras.approve');
-    // Route download PDF
-    Route::get('/dehum-matras/{id}/download-pdf', [DehumMatrasController::class, 'downloadPdf'])->name('dehum-matras.downloadPdf');
-
-    // Route Mesin Caplining
-    Route::get('/caplining', [CapliningController::class, 'index'])->name('caplining.index');
-    Route::get('/caplining/create', [CapliningController::class, 'create'])->name('caplining.create');
-    Route::post('/caplining', [CapliningController::class, 'store'])->name('caplining.store');
-    Route::get('/caplining/{id}/edit', [CapliningController::class, 'edit'])->name('caplining.edit');
-    Route::put('/caplining/{id}', [CapliningController::class, 'update'])->name('caplining.update');
-    Route::get('/caplining/{id}', [CapliningController::class, 'show'])->name('caplining.show');
-    Route::post('/caplining/{id}/approve', [CapliningController::class, 'approve'])->name('caplining.approve');
-    // Route download PDF
-    Route::get('/caplining/{id}/download-pdf', [CapliningController::class, 'downloadPdf'])->name('caplining.downloadPdf');
-
-    // Route Mesin Vacum Cleaner
-    Route::get('/vacuum-cleaner', [VacumCleanerController::class, 'index'])->name('vacuum-cleaner.index');
-    Route::get('/vacuum-cleaner/create', [VacumCleanerController::class, 'create'])->name('vacuum-cleaner.create');
-    Route::post('/vacuum-cleaner', [VacumCleanerController::class, 'store'])->name('vacuum-cleaner.store');
-    Route::get('/vacuum-cleaner/{id}/edit', [VacumCleanerController::class, 'edit'])->name('vacuum-cleaner.edit');
-    Route::put('/vacuum-cleaner/{id}', [VacumCleanerController::class, 'update'])->name('vacuum-cleaner.update');
-    Route::get('/vacuum-cleaner/{id}', [VacumCleanerController::class, 'show'])->name('vacuum-cleaner.show');
-    Route::post('/vacuum-cleaner/{id}/approve', [VacumCleanerController::class, 'approve'])->name('vacuum-cleaner.approve');
-    // Route download PDF
-    Route::get('/vacuum-cleaner/{id}/download-pdf', [VacumCleanerController::class, 'downloadPdf'])->name('vacuum-cleaner.downloadPdf');
-
-    // Route Mesin Slitting 
-    Route::get('/slitting', [SlittingController::class, 'index'])->name('slitting.index');
-    Route::get('/slitting/create', [SlittingController::class, 'create'])->name('slitting.create');
-    Route::post('/slitting', [SlittingController::class, 'store'])->name('slitting.store');
-    Route::get('/slitting/{id}/edit', [SlittingController::class, 'edit'])->name('slitting.edit');
-    Route::put('/slitting/{id}', [SlittingController::class, 'update'])->name('slitting.update');
-    Route::get('/slitting/{id}', [SlittingController::class, 'show'])->name('slitting.show');
-    Route::post('/slitting/{id}/approve', [SlittingController::class, 'approve'])->name('slitting.approve');
-    // Route download PDF
-    Route::get('/slitting/{id}/download-pdf', [SlittingController::class, 'downloadPdf'])->name('slitting.downloadPdf');
 });
