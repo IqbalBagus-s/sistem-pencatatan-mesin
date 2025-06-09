8ac4eb509630469bf2178b3b41d4f1f497763510
iqbal.bs_ <119472842+IqbalBagus-s@users.noreply.github.com>
2025-06-09 21:56:16 +0700

    feat: Added the Hashidable traits function to work on models (completed for airdryer, waterchiller, compressor, grinder, dehum bahan, hopper, dehum matras, autoloader, caplining, slitting, vacuum cleaner, and crane matras).

--- app/Models/CapliningCheck.php
+++ app/Models/CapliningCheck.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\Hashidable;
 
 class CapliningCheck extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, Hashidable;
 
     protected $table = 'caplining_checks';
 
