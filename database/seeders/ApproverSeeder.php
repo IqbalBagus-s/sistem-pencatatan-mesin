d9a86d8f56dce79a105cac7dadc9add39670033d
iqbal.bs_ <119472842+IqbalBagus-s@users.noreply.github.com>
2025-06-11 22:05:28 +0700

    refactor: Updated the seeder by adding data to the database.

--- database/seeders/ApproverSeeder.php
+++ database/seeders/ApproverSeeder.php
@@ -15,21 +15,48 @@ class ApproverSeeder extends Seeder
     {
         DB::table('approvers')->insert([
             [
-                'username' => 'jeri',
-                'password' => Hash::make('123456'), // bcrypt password
+                'username' => 'Approver1',
+                'password' => Hash::make('approver1123'),
                 'role' => 'Penanggung Jawab',
                 'status' => 'aktif',
-                'created_at' => now(),
-                'updated_at' => now(),
+                'created_at' => '2025-05-08 07:15:33',
+                'updated_at' => '2025-05-08 07:15:33',
                 'deleted_at' => null,
             ],
             [
-                'username' => 'samul',
-                'password' => Hash::make('123456'), // bcrypt password
+                'username' => 'Dewi',
+                'password' => Hash::make('dewi123'),
                 'role' => 'Penanggung Jawab',
                 'status' => 'aktif',
-                'created_at' => now(),
-                'updated_at' => now(),
+                'created_at' => '2025-05-13 03:03:03',
+                'updated_at' => '2025-05-13 03:03:03',
+                'deleted_at' => null,
+            ],
+            [
+                'username' => 'Abdul',
+                'password' => Hash::make('abdul123'),
+                'role' => 'Penanggung Jawab',
+                'status' => 'aktif',
+                'created_at' => '2025-05-13 03:46:11',
+                'updated_at' => '2025-05-13 03:46:11',
+                'deleted_at' => null,
+            ],
+            [
+                'username' => 'Bismar',
+                'password' => Hash::make('bismar123'),
+                'role' => 'Penanggung Jawab',
+                'status' => 'aktif',
+                'created_at' => '2025-05-13 03:48:56',
+                'updated_at' => '2025-05-13 03:48:56',
+                'deleted_at' => null,
+            ],
+            [
+                'username' => 'IT',
+                'password' => Hash::make('it123'),
+                'role' => 'Penanggung Jawab',
+                'status' => 'aktif',
+                'created_at' => '2025-05-13 08:20:02',
+                'updated_at' => '2025-05-13 08:20:02',
                 'deleted_at' => null,
             ]
         ]);
--- database/seeders/FormSeeder.php
+++ database/seeders/FormSeeder.php
@@ -15,6 +15,7 @@ class FormSeeder extends Seeder
     public function run(): void
     {
         $forms = [
+            // 1
             [
                 'nomor_form' => 'APTEK/006/REV.01',
                 'nama_form' => 'PENGECEKAN VACUUM CLEANER',
@@ -22,6 +23,7 @@ class FormSeeder extends Seeder
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
+            // 2
             [
                 'nomor_form' => 'APTEK/014/REV.00',
                 'nama_form' => 'PENGECEKAN MESIN SLEETING',
@@ -29,6 +31,7 @@ class FormSeeder extends Seeder
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
+            // 3
             [
                 'nomor_form' => 'APTEK/048/REV.01',
                 'nama_form' => 'CHECKLIST DEHUM MATRAS',
@@ -36,6 +39,7 @@ class FormSeeder extends Seeder
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
+            // 4
             [
                 'nomor_form' => 'APTEK/019/REV.02',
                 'nama_form' => 'PENGECEKAN MESIN AIR DRYER',
@@ -43,6 +47,7 @@ class FormSeeder extends Seeder
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
+            // 5
             [
                 'nomor_form' => 'APTEK/023/REV.01',
                 'nama_form' => 'PENGECEKAN CHILLER',
@@ -50,6 +55,7 @@ class FormSeeder extends Seeder
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
+            // 6
             [
                 'nomor_form' => 'APTEK/015/REV.04',
                 'nama_form' => 'PEMERIKSAAN MESIN GILING',
@@ -57,6 +63,7 @@ class FormSeeder extends Seeder
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
+            // 7
             [
                 'nomor_form' => 'APTEK/047/REV.01',
                 'nama_form' => 'PENGECEKAN HOPPER',
@@ -64,6 +71,7 @@ class FormSeeder extends Seeder
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
+            // 8
             [
                 'nomor_form' => 'APTEK/035/REV.02',
                 'nama_form' => 'PENGECEKAN DEHUM BAHAN',
@@ -71,6 +79,7 @@ class FormSeeder extends Seeder
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
+            // 9
             [
                 'nomor_form' => 'APTEK/041/REV.00',
                 'nama_form' => 'FORM PEMERIKSAAN COMPRESSOR',
@@ -78,6 +87,7 @@ class FormSeeder extends Seeder
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
+            // 10
             [
                 'nomor_form' => 'APTEK/046/REV.01',
                 'nama_form' => 'FORM PENGECEKAN AUTOLOADER',
@@ -85,13 +95,15 @@ class FormSeeder extends Seeder
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
+            // 11
             [
                 'nomor_form' => 'APTEK/016/REV.01',
-                'nama_form' => 'FORM PENGECEKAN MESIN CAPLINGIN',
+                'nama_form' => 'FORM PENGECEKAN MESIN CAPLINING',
                 'tanggal_efektif' => '2022-10-19 00:00:00',
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
+            // 12
             [
                 'nomor_form' => 'APTEK/005/REV.00',
                 'nama_form' => 'CHECKLIST PERAWATAN CRANE MATRAS',
